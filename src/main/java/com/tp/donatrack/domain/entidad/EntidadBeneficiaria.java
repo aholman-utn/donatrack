7adff7afd938c9b9484a7701e9cf71875b02e55a
Ignacio Durandez <idurandez@frba.utn.edu.ar>
2026-05-13 19:10:44 -0300

    completo el codigo de EntidadBeneficiaria para que pueda operar con los test

--- src/main/java/com/tp/donatrack/domain/entidad/EntidadBeneficiaria.java
+++ src/main/java/com/tp/donatrack/domain/entidad/EntidadBeneficiaria.java
@@ -1,13 +1,14 @@
 package com.tp.donatrack.domain.entidad;
 
-import com.tp.donatrack.domain.persona.Persona;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.tp.donatrack.domain.bien.Bien;
-import com.tp.donatrack.domain.donacion.Donacion;
+import com.tp.donatrack.domain.donacion.DonacionSegmentada;
+import com.tp.donatrack.domain.persona.Persona;
 
 import lombok.Getter;
 import lombok.Setter;
-import java.util.List;
-import java.util.ArrayList;
 
 @Getter
 @Setter
@@ -18,8 +19,6 @@ public class EntidadBeneficiaria {
     private Persona persona;
     private List<Necesidad> necesidades = new ArrayList<>();
 
-
-    /*
     public void implementarDonacion(Donacion donacion) {
         Necesidad nuevaNecesidad = new Necesidad(
             donacion.getCantidad(), 
@@ -29,5 +28,25 @@ public class EntidadBeneficiaria {
         
         this.necesidades.remove(nuevaNecesidad);
     }
-    */
+    public void implementarDonacion(DonacionSegmentada donacion) {
+        Necesidad nuevaNecesidad = new Necesidad(
+            donacion.getCantidad(), 
+            donacion.getSubCategoria(), 
+            donacion.getBienes()
+        );
+        
+        boolean existeNecesidad = this.necesidades.stream().anyMatch(necesidad -> 
+            necesidad.cantidad().equals(nuevaNecesidad.cantidad()) && 
+            necesidad.subCategoria().equals(nuevaNecesidad.subCategoria())
+        );
+        
+         if(this.necesidades.contains(nuevaNecesidad)){
+            this.necesidades.remove(nuevaNecesidad);    
+        }else{
+            throw new RuntimeException("No existe esa necesidad en la lista de requerimientos");
+        }
+
+    public Integer getCantNecesidades(){
+        return this.necesidades.size();
+    }
 }
